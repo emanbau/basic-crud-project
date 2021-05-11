dabdcf19eedf7db76580266067be9c9746cc1b49
Eman Bau <emanbautista410@gmail.com>
2021-05-11 19:55:12 -0400

    Signup

---
+++ server/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.scss';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from
+}
+from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import Signup from './Pages/Signup';
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`GraphQL error ${message}`);
+      return null;
+    });
+  }
+});
+
+const link = from([
+  errorLink, 
+  new HttpLink({ uri: "http://localhost:3001/graphql"})
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Signup />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,8 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const cors = require('cors');
-const { createConnection } = require('typeorm');
-const schema = require('./Schema');
-const { Users } = require('./Entities/Users');
-const PORT = 3001
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const main = async function() {
-
-    await createConnection({
-        type: "mysql",
-        host: "localhost",
-        port: 3306,
-        database: "GraphQLCRUD",
-        username: "root",
-        password: "born1968",
-        logging: true,
-        synchronize: false,
-        entities: [Users],
-    })
-
-    const app = express();
-    app.use(cors());
-
-    app.use(express.json());
-    app.use('/graphql', graphqlHTTP({
-        schema,
-        graphiql: true
-    }))
-
-    app.listen(PORT, () => console.log("Server is running!"))
-    
-}
-
-main().catch((err) => console.log(err));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
